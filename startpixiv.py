ab17fff34b6b0d6b147caab1fbe5ffe4afe03b82
Meteor <coopticks@outlook.com>
2022-02-12 14:33:15 +0800

    Add files via upload

--- startpixiv.py
+++ startpixiv.py
@@ -13,9 +13,16 @@ from urllib import parse
 recommendList = None
 updateTime = 0
 
+"""
+获取Pixiv图片Base64值
+
+参数：picPath，Pixiv图片链接
+返回：图片base64 string
+
+"""
 def getImage(picPath):
     headers = {
-               'user-agent': 'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+               'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
                'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
                'sec-fetch-site': 'cross-site',
                'sec-fetch-mode': 'no-cors',
@@ -25,17 +32,31 @@ def getImage(picPath):
     }
     r = requests.get(url=picPath, headers=headers)
     return base64.b64encode(r.content).decode('utf-8')
+"""
+获取图片Base64值
+参数：url，任意图片链接
+返回：图片base64 string
+
+"""
 def getPin(url):
     r = requests.get(url)
     return base64.b64encode(r.content).decode('utf-8')
+"""
+获取Pixiv日排行榜
+"""
 def getList():
     r = requests.get("https://www.pixiv.net/touch/ajax/recommender/top?limit=500&lang=jp")
     message = r.json()
     return message["body"]["related"]
-
+"""
+检测多少分钟后
+"""
 def event_minute_later(event, timeout):
     return event + timeout < time.time()
 
+"""
+获取Pixiv日排行榜前500个随机一个图片Base64 String值
+"""
 def getTop():
     global updateTime 
     global recommendList
@@ -53,6 +74,14 @@ def getTop():
     message = r.json()
     return getImage(message["body"]["illust_details"][0]["url"])
 
+"""
+获取指定画师id随机一个画作图片Base64 值
+返回：JSON_OBJECT(
+    "b64"：画作Base64值
+    "uid": 画师id
+    "uname": 画师名称
+)
+"""
 def SearchPainter(id):
     r = requests.get("https://www.pixiv.net/touch/ajax/user/illusts?id={}&type=illust&lang=en".format(id))
     message = r.json()
@@ -71,11 +100,17 @@ def SearchPainter(id):
         }
     })
 
+"""
+从指定画师id List中抽取一个并获取随机画作
+"""
 def getRandom():
     userlen = len(userid) - 1
     uid = userid[randrange(0, userlen)]
     return SearchPainter(uid)
 
+"""
+根据关键词搜索Pixiv，并抽取随机一个画作，返回其Base64 值
+"""
 def getName(name, num):
     r = requests.get(url='https://www.pixiv.net/touch/ajax/search/illusts?include_meta=0&type=illust_and_ugoira&s_mode=s_tag_full&word={}&lang=en'.format(name))
     message = r.json()
@@ -95,6 +130,9 @@ def getName(name, num):
         })
     return json.dumps(b64lists)
 
+"""
+根据关键词搜索Pinterest，并随机抽取一个，返回其Base64值
+"""
 def getPinterest(name, num):
     payload = json.dumps(
         {
@@ -119,7 +157,19 @@ def getPinterest(name, num):
             except:
                 return json.dumps({"error": "关键词{}不存在！".format(name)})
     return json.dumps(b64list)
+"""
+解析微博链接，返回作者姓名，内容，图片，及视频
 
+返回格式：
+JSON_OBJECT(
+    "author": 作者姓名
+    "content": 内容
+    "b64": JSON_ARRAY(
+        图片Base64值
+    )
+    "video"：视频地址
+)
+"""
 def getWeibo(link):
     if "weibo.com" in link:
         link = re.sub(r"(www\.)?weibo\.com", "m.weibo.cn", link)
